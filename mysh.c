fff447822d5e56c459ee4b46479cd45adbb6f187
Korey Lombardi <koreylombardi17@yahoo.com>
2021-10-10 10:55:11 -0400

    Added todo's

--- mysh.c
+++ mysh.c
@@ -10,25 +10,28 @@ typedef struct Buffer Buffer;
 // ArrayList architecture
 typedef struct Buffer {
     	int size, cap;
-	char ** arr;
+	char** arr;
 } Buffer;
 
 // Data structure prototypes
 Buffer * createBuffer();
-void appendCommandToBuffer(Buffer *, char *);
-void expandBuffer(Buffer *);
-char** createArgsArray(int);
+void appendCommandToBuffer(Buffer*, char*);
+void expandBuffer(Buffer*);
+char** createArgsArray(int, int);
 
 // Helper function prototypes
-void printCommands(Buffer *);
-int doesDirectoryExist(char *, struct stat);
-void printString(char *);
-char** commandDelimiter(char []);
-int countArgs(char []);
+void printCommands(Buffer*);
+int doesDirectoryExist(char*, struct stat);
+void printString(char*);
+char** commandDelimiter(char*);
+int countArgs(char*);
+int countLongestArg(char*);
 
 // Shell command prototypes
-void whereami(char *);
-void start(char *);
+void whereami(char*);
+void start(char*);
+int background(char*);
+int dalek(int);
 
 int main() {
 	// Buffer to store recent commands
@@ -40,10 +43,20 @@ int main() {
 
 	start("/usr/bin/vim");
 
+	// Dont forget to free memory
+	
 	return 0;
 }
 
+// Prints to the terminal current directory
+// TODO: Make function return int
+void whereami(char* currentdir) {
+	printString(currentdir);
+}
+
 // Starts a program with or without parameters
+// TODO: Make function return int
+// TODO: Test function with multiple args
 void start(char * command){
 	// pid uses this value behind the scenes
 	int status;
@@ -61,17 +74,28 @@ void start(char * command){
 	}
 }
 
+// TODO: Implement function
+int background(char* command) {
+
+}
+
+// TODO: Implement function
+int dalek(int pid){
+
+}
+
 // Splits a command into an array of all of its args
-char ** commandDelimiter(char command[]) {
+char** commandDelimiter(char* command) {
 	char delimeter [] = " ";
 	char argument [50];
 	int argsIndex = 0;
 	int i = 0;
 	
 	// Pointer used to iterate through through command stopping at all spaces
-	char * ptr = strtok(command, delimeter);
+	char* ptr = strtok(command, delimeter);
 	int numberArgs = countArgs(command);
-	char** usersArgs = createArgsArray(numberArgs);
+	int maxArgLength = countLongestArg(command);
+	char** usersArgs = createArgsArray(numberArgs, maxArgLength);
 
 	// Split string wherever there are spaces
 	while(ptr != NULL) {
@@ -94,10 +118,9 @@ char ** commandDelimiter(char command[]) {
 }
 
 // Returns the number of args contained in command
-int countArgs(char command[]) {
+int countArgs(char* command) {
 	char delimeter[] = " ";
 	int argsCounter = 0;
-	char** createArgsArray(int numberArgs);
 	// Pointer used to iterate through through command, stopping at all spaces
 	char * ptr = strtok(command, delimeter);	
 	// Count number of strings in command	
@@ -112,22 +135,40 @@ int countArgs(char command[]) {
 	return (argsCounter+1);
 }
 
+// Returns the length of the longest arg
+int countLongestArg(char* command) {
+	char delimeter[] = " ";
+	int maxArgLength = 0;
+	int argLength = 0;
+	// Pointer used to iterate through through command, stopping at all spaces
+	char * ptr = strtok(command, delimeter);	
+	// Count number of strings in command	
+	while(ptr != NULL) {
+		while(*ptr != '\0'){
+			ptr++;
+			argLength++;
+		}
+		if(argLength > maxArgLength) {
+			maxArgLength = argLength;
+		}
+		argLength = 0;
+		ptr = strtok(NULL, delimeter);
+	}
+	// +1 for NULL character value at end of string
+	return (maxArgLength+1);
+}
+
 // Function allocates memory for array of args
-char** createArgsArray(int numberArgs) {
+char** createArgsArray(int numberArgs, int maxArgLength) {
 	char** argsArray = (char**)malloc(sizeof(char*));
 	for(int i = 0; i < numberArgs; i++) {
-		argsArray[i] = (char*)malloc(50*sizeof(char));
+		argsArray[i] = (char*)malloc(maxArgLength*sizeof(char));
 	}
 	return argsArray;
 }
 
-// Prints to the terminal current directory
-void whereami(char * currentdir) {
-	printString(currentdir);
-}
-
 // Create the buffer to store the commands
-Buffer * createBuffer() {
+Buffer* createBuffer() {
 	Buffer * ret = calloc(1, sizeof(Buffer));
 	ret->size = 0;
 	ret->cap = 8;
@@ -136,7 +177,7 @@ Buffer * createBuffer() {
 }
 
 // Append command to Buffer
-void appendCommandToBuffer(Buffer * buffer, char * command) {
+void appendCommandToBuffer(Buffer* buffer, char* command) {
     // Location where command will be appended
 	int index = buffer->size;
 
@@ -150,21 +191,21 @@ void appendCommandToBuffer(Buffer * buffer, char * command) {
 }
 
 // Reallocate memory by a multiple of two
-void expandBuffer(Buffer * buffer) {
+void expandBuffer(Buffer* buffer) {
     buffer->cap = buffer->cap * 2;
-	char ** largerArr = realloc(buffer->arr, buffer->cap * sizeof(char *));
+	char** largerArr = realloc(buffer->arr, buffer->cap * sizeof(char *));
 	buffer->arr = largerArr;
 }
 
 // Print the Commands 
-void printCommands(Buffer * buffer) {
+void printCommands(Buffer* buffer) {
 	for(int i = 0; i < buffer->size; i++) {
 		printString(buffer->arr[i]);
 	}
 }
 
 // Returns 1 if directory exist, returns 0 if does NOT exist
-int doesDirectoryExist(char * directory, struct stat s) {
+int doesDirectoryExist(char* directory, struct stat s) {
 	int status = stat(directory, &s);
 	if(status == -1) {
 		return 0;
@@ -174,7 +215,7 @@ int doesDirectoryExist(char * directory, struct stat s) {
 }
 
 // Print a string
-void printString(char * str) {
+void printString(char* str) {
 	printf("%s", str);
 	printf("\n");
 }
